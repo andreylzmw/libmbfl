cb2a98def64de74166c1df9e45e60a280735ab1c
Andrey Arutiunian <andreylzmw@gmail.com>
2023-04-16 16:58:23 +0300

    try fix build

--- include/kphp/libmbfl/filters/mbfilter_utf8_mobile.h
+++ include/kphp/libmbfl/filters/mbfilter_utf8_mobile.h
@@ -51,5 +51,6 @@ extern const struct mbfl_convert_vtbl vtbl_wchar_utf8_sb;
 
 int mbfl_filt_conv_utf8_mobile_wchar(int c, mbfl_convert_filter *filter);
 int mbfl_filt_conv_wchar_utf8_mobile(int c, mbfl_convert_filter *filter);
+int mbfl_filt_put_invalid_char(int c, mbfl_convert_filter *filter);
 
 #endif /* MBFL_MBFILTER_UTF8_MOBILE_H */
